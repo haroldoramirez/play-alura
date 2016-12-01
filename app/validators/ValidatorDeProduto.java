b7e99ce0ac6fe9295d47aa48805e3db0306de672
Haroldo Ramirez <haroldoramirez@gmail.com>
2016-12-01 17:53:12 -0200

    resolvido problema do validador de produto

--- app/validators/ValidatorDeProduto.java
+++ app/validators/ValidatorDeProduto.java
@@ -14,21 +14,25 @@ public class ValidatorDeProduto {
 
     public boolean temErros(Form<Produto> formulario) {
 
+        String codigo = formulario.field("codigo").value();
+
+        double preco = -1.0;
+
         try {
-            Produto produto = formulario.get();
-
-            if (produto.getPreco() < 0.0) {
-                formulario.reject(new ValidationError("preco", "O preço do produto deve ser maior que 0!"));
-            }
-
-            if (produtoDAO.comCodigo(produto.getCodigo()).isPresent()) {
-                formulario.reject(new ValidationError("codigo", "Já existe um produto com este código!"));
-            }
-
-            return formulario.hasErrors();
+            preco = Double.parseDouble(formulario.field("preco").value());
         } catch (Exception e) {
-            return formulario.hasErrors();
+            formulario.reject(new ValidationError("preco", "O preço do produto deve ser um número!"));
         }
+
+        if (preco < 0.0) {
+            formulario.reject(new ValidationError("preco", "O preço do produto deve ser maior que 0!"));
+        }
+
+        if (produtoDAO.comCodigo(codigo).isPresent()) {
+            formulario.reject(new ValidationError("codigo", "Já existe um produto com este código!"));
+        }
+
+        return formulario.hasErrors();
 
     }
 
